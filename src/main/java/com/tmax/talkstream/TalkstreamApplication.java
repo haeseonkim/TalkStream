635dd419b7181023508137fe9b275c85e01b64ed
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-10-03 20:17:59 +0900

    init

---
+++ src/main/java/com/tmax/talkstream/TalkstreamApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.talkstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkstreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TalkstreamApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tmax/talkstream/TalkstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.talkstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkstreamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
