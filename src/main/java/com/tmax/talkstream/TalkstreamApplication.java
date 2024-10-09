f49e69872c5346459ca39fdbf6367e7ba979ed21
haeseonkim <khs51777@naver.com>
2024-10-09 18:21:53 +0900

    chore: db-local-setup

--- src/main/java/com/tmax/talkstream/TalkstreamApplication.java
+++ src/main/java/com/tmax/talkstream/TalkstreamApplication.java
@@ -2,9 +2,8 @@ package com.tmax.talkstream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TalkstreamApplication {
 
   public static void main(String[] args) {
