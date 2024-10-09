db2ecf0f3dd8507d136b7c7f5ab8c85d46ddfa4f
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-10-09 17:04:12 +0900

    feat: 데이터베이스 미사용 시 불필요한 설정 자동 구성을 제외

--- src/main/java/com/tmax/talkstream/TalkstreamApplication.java
+++ src/main/java/com/tmax/talkstream/TalkstreamApplication.java
@@ -2,8 +2,9 @@ package com.tmax.talkstream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TalkstreamApplication {
 
   public static void main(String[] args) {
