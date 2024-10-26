2b2babdb7bbba7202d7ca97b52d76ccc90cdf4d5
haeseonkim <khs51777@naver.com>
2024-10-26 19:40:17 +0900

    feat: RoomRepository에 @NonNull 적용 (null-safety 강화)

--- src/main/java/com/tmax/talkstream/repository/RoomRepository.java
+++ src/main/java/com/tmax/talkstream/repository/RoomRepository.java
@@ -4,9 +4,11 @@ import com.tmax.talkstream.model.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository  extends JpaRepository<Room, Long> {
-    Page<Room> findAll(Pageable pageable);
+    @NonNull
+    Page<Room> findAll(@NonNull Pageable pageable);
 }
